f386f4c56cda484cabd8e418fbce8631fe43b012
Tyler B Yeager <wow_d2@hotmail.com>
2021-09-30 23:36:47 -0700

    obtain value from textarea

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -25,7 +25,7 @@ setInterval(liveTime, 500);
 var dateInput = $("#datepicker");
 var textBox = $("textarea");
 var saveInput = $(".saveBtn");
-console.log(textBox);//log test on textBox
+//console.log(textBox);//log test on textBox
 //console.log(saveInput);//log test on saveInput
 
 //saveInput.addEventListener("click", saveEvent); //to be used later
@@ -36,8 +36,16 @@ $( function() { //added date picker widget from jqueryui.com
     $( "#datepicker" ).datepicker();
 } );
 
-function saveEvent() {
-    for (var i = 0; i < textBox.length; i++) {
-        console.log(textBox[i]); //returning textarea but no input
-    }
-}
+
+
+
+$('textarea').each(function(saveEvent) {
+    let userInput = [];
+    $(textBox).each(function (index, element) { // this will display the textarea's text which should be blank
+    console.log(`textarea${index}: ${$(element).text()}`); // this will grab the value of the current textarea
+    let value = $(element).val(); //display the value
+    console.log(`Value: ${value}`); // put the current value into the array
+    userInput.push(value);
+    localStorage.setItem("value", JSON.stringify(userInput));
+    })
+});
