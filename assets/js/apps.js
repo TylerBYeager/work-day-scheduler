c96c5dee47b4e2702944d215986d0be6760ccecf
Tyler B Yeager <wow_d2@hotmail.com>
2021-09-30 20:07:18 -0700

    add live date and time

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,13 +1,22 @@
 $(document).ready(function () {
 
-console.log(moment()); //confirming that moment is working. 
+
 
 //console.log(moment().format());
 
-$("#today").text(moment().format("dddd, MMMM, Do, YYYY, h:mm:ss a"));
+//$("#today").text(moment().format("dddd, MMMM, Do, YYYY, h:mm:ss a"));
 //places current time and date on the html
 
+function liveTime() {
+    var currentTime = moment().format("dddd, MMMM, Do, YYYY, h:mm:ss a")
+    $("#today").text(currentTime)
+} //function sets the current date and time. Set at an interval of a half a second. 
 
 
+
+liveTime();
+
+
+setInterval(liveTime, 500);
 
 })
