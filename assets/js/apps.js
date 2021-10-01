f19e88c5e07369d5624f407e99725bd176127d50
Tyler B Yeager <wow_d2@hotmail.com>
2021-09-30 22:16:44 -0700

    add save button function

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
 
-
 //console.log(moment().format());
 
 //$("#today").text(moment().format("dddd, MMMM, Do, YYYY, h:mm:ss a"));
@@ -24,8 +23,21 @@ setInterval(liveTime, 500);
 
 
 var dateInput = $("#datepicker");
+var textBox = $("textarea");
+var saveInput = $(".saveBtn");
+console.log(textBox);//log test on textBox
+//console.log(saveInput);//log test on saveInput
+
+//saveInput.addEventListener("click", saveEvent); //to be used later
+
 
 
 $( function() { //added date picker widget from jqueryui.com
     $( "#datepicker" ).datepicker();
 } );
+
+function saveEvent() {
+    for (var i = 0; i < textBox.length; i++) {
+        console.log(textBox[i]); //returning textarea but no input
+    }
+}
